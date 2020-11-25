5e05f74e89a77bc1855720f11ba0b2fd2616b016
AomAryan <74653642+AomAryan@users.noreply.github.com>
2020-11-25 14:56:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var rect1,rect2;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  rect1 = createSprite(400, 200, 50, 50);
+  rect1.shapeColor = "yellow";
+  rect2 = createSprite( 400,200, 50, 50);
+  rect2.shapeColor = "orange";
+}
+
+function draw() {
+  background(250,205,55); 
+  rect1.x = World.mouseX;
+  rect1.y = World.mouseY; 
+console.log("rect1" + rect1.x + rect1.y);
+console.log("rect2" + rect2.x + rect2.y);
+
+//code for algorithms (isTouching).
+if(rect1.x - rect2.x <= rect1.width/2 + rect2.width/2 && 
+    rect2.x - rect1.x <= rect2.width/2 + rect1.width/2 && 
+    rect1.y - rect2.y <= rect1.height/2 + rect2.height/2 &&
+    rect2.y - rect1.y <= rect1.height/2 + rect2.height/2 ){
+rect1.shapeColor = "green";
+rect2.shapeColor = "green";
+
+
+
+}
+else{
+  rect1.shapeColor = "blue";
+rect2.shapeColor = "blue";
+}
+  drawSprites();
+}
